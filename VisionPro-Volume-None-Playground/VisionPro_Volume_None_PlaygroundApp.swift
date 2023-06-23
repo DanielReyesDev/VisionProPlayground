e23621499ade387768c61f4e3cb010a8a1de84b1
Daniel Reyes <daniel.reyes@wizeline.com>
2023-06-22 22:03:33 -0600

    Initial Commit

---
+++ VisionPro-Volume-None-Playground/VisionPro_Volume_None_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionPro_Volume_None_PlaygroundApp.swift
+//  VisionPro-Volume-None-Playground
+//
+//  Created by Daniel Reyes on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionPro_Volume_None_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
